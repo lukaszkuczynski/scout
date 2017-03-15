bc138f0b33e90019a74428b4c77ea736a1cc181e
lukaszkuczynski <kuczynskilukasz@gmail.com>
2017-03-15 17:17:20 +0100

    in-memory notes

--- application/notes.py
+++ application/notes.py
@@ -1,3 +1,29 @@
+from abc import abstractmethod
+
+
 class Notes:
     def __init__(self):
         self.previous_value = ''
+
+    @abstractmethod
+    def save_for_mission(self, mission, notes):
+        pass
+
+    def read_for_mission(self, mission):
+        pass
+
+
+class InMemoryNotes(Notes):
+
+    def __init__(self):
+        self.notes = {}
+
+    def add_for_mission(self, mission, notes):
+        if not mission['id'] in self.notes:
+            self.notes[mission['id']] = []
+        self.notes[mission['id']].append(notes)
+
+    def read_for_mission(self, mission):
+        id = mission['id']
+        return self.notes[id]
+
---
+++ test/test_inmemory_notes.py
@@ -0,0 +1,21 @@
+from unittest import main, TestCase
+from application.notes import InMemoryNotes
+
+
+class InMemoryNotesTest(TestCase):
+
+    def test_notes_after_add_contains_notes(self):
+        notes = InMemoryNotes()
+
+        mission = {
+            'id': 'this_is_id'
+        }
+        note = 'This is good note'
+
+        notes.add_for_mission(mission, note)
+        notes_read = notes.read_for_mission(mission)
+        self.assertIn('This is good note', notes_read)
+
+
+if __name__ == '__main__':
+    main()
