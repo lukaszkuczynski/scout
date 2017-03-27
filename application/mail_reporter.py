00d941579a37afc5724ed66352ed547b3e1aea27
lukaszkuczynski <kuczynskilukasz@gmail.com>
2017-03-27 18:42:08 +0200

    mailing mixed

--- application/mail_reporter.py
+++ application/mail_reporter.py
@@ -5,21 +5,27 @@ from email.mime.text import MIMEText
 
 
 class MailReporter(Reporter):
-    def __init__(self):
+    def __init__(self, config):
         self.smtp_server = 'smtp.gmail.com:587'
-        if not 'SMTP_LOGIN_USER' in environ:
-            raise Exception("Scout requires SMTP_LOGIN_USER to be set")
-        self.smtp_user = environ['SMTP_LOGIN_USER']
-        self.smtp_password = ''
+        if 'SCOUT_SMTP_HOST' in environ:
+            self.smtp_server = environ['SCOUT_SMTP_HOST']
+        if not 'SCOUT_SMTP_USER' in environ:
+            raise Exception("Scout requires SCOUT_SMTP_USER to be set")
+        if not 'SCOUT_SMTP_PASSWORD' in environ:
+            raise Exception("Scout requires SCOUT_SMTP_PASSWORD to be set")
+        self.smtp_user = environ['SCOUT_SMTP_USER']
+        self.smtp_password = environ['SCOUT_SMTP_PASSWORD']
         self.smtp = smtplib.SMTP(self.smtp_server)
+        self.config = config
 
 
     def report(self, value):
         mime = MIMEText(value)
         mime['Subject'] = 'Scout mission report'
         mime['From'] = "scout@scout"
-        mime['To'] = "kuczynskilukasz@gmail.com"
+        mime['To'] = ','.join(self.config['to'])
         self.smtp.ehlo()
         self.smtp.starttls()
+        self.smtp.login(self.smtp_user, self.smtp_password)
         self.smtp.send_message(mime)
         self.smtp.quit()
